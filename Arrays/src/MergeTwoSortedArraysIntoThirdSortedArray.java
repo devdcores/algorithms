b47a05da6e2516cef6b65f57c5ea8917d4fee5bc
devaraj reddy <devaraj.reddy@siemens.com>
2018-03-21 21:12:51 +0530

    MergeTwoSortedArraysIntoThirdSortedArray Code Commit.

--- Arrays/src/MergeTwoSortedArraysIntoThirdSortedArray.java
+++ Arrays/src/MergeTwoSortedArraysIntoThirdSortedArray.java
@@ -1,4 +1,53 @@
+import java.util.Arrays;
+
+/**
+ * Created by Devaraj Reddy, 3/21/2018 - 8:22 PM
+ */
 public class MergeTwoSortedArraysIntoThirdSortedArray {
 
-}
+    /*
+    *
+    * Logic: traverse through both the arrays at same time, compare the values which ever value is lesser put into new resultant array.
+    * for remaining elements traverse through remaining elements and copy to resultant array.
+    *
+    * */
+    public static void main(String[] args) {
 
+        int[] sortedArray1 = {2, 4, 7, 8, 11};
+        int[] sortedArray2 = {3, 6, 9, 13, 17, 19, 23, 34, 45};
+        final int s1length = sortedArray1.length;
+        final int s2length = sortedArray2.length;
+
+        int[] resultMergedSortedArray = new int[s1length + s2length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < s1length && j < s2length) {
+
+            if (sortedArray1[i] < sortedArray2[j]) {
+                resultMergedSortedArray[k] = sortedArray1[i];
+                i++;
+                k++;
+            } else {
+                resultMergedSortedArray[k] = sortedArray2[j];
+                j++;
+                k++;
+            }
+
+        }
+
+        while (i < s1length) {
+            resultMergedSortedArray[k] = sortedArray1[i];
+            i++;
+            k++;
+        }
+
+        while (j < s2length) {
+            resultMergedSortedArray[k] = sortedArray2[j];
+            j++;
+            k++;
+        }
+
+        System.out.println("Resultant Array : "+ Arrays.toString(resultMergedSortedArray));
+    }
+
+}
